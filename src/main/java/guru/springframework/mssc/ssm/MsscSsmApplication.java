34988b45bfb487cee2a6782ab5ecda9b7cdbb3b7
to20291063 <tod.barnett@wipro.com>
2022-02-17 17:24:07 +0000

    initial pom

---
+++ src/main/java/guru/springframework/mssc/ssm/MsscSsmApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.mssc.ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscSsmApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MsscSsmApplication.class, args);
+    }
+
+}
